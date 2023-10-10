bc5e72f20670883a0353759ce09a77b1f55b37de
Regis Melo <regisoliveiramelo@gmail.com>
2023-10-10 14:56:09 -0300

    change migration

--- db/migrations/20231009222839_create-users.ts
+++ db/migrations/20231009222839_create-users.ts
@@ -4,6 +4,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
+    table.foreign('id').references('meals.user_id')
     table.text('user_name').notNullable()
     table.uuid('session_id').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
